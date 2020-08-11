1969f8e9c996f765767e1d3dce864f13beaead5b
Luke Shumaker <lukeshu@datawire.io>
2020-08-11 16:19:04 -0600

    infra: Have envoy.service.auth.v2alpha be less special [ci-skip]
    Now the only special thing about it should be a "magic" comment in
    `v3/external_auth.proto`.
    
    I did not run any of the proto_format.sh or check_format.py scripts
    yet; I'm leaving that for a separate commit for review purposes.

--- tools/proto_format/proto_sync.py
+++ tools/proto_format/proto_sync.py
@@ -398,9 +398,6 @@ def generate_current_api_dir(api_dir, dst_dir):
 
     for p in dst.glob('**/*.md'):
         p.unlink()
-    # envoy.service.auth.v2alpha exist for compatibility while we don't run in protoxform
-    # so we ignore it here.
-    shutil.rmtree(str(dst.joinpath("service", "auth", "v2alpha")))
 
     for proto in IGNORED_V2_PROTOS:
         shutil.rmtree(str(dst.joinpath(proto[6:])))
