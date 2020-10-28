99d35db1ab4224d405a114456c77502e7b1df615
John Esmet <john.esmet@gmail.com>
2020-10-28 20:37:07 +0000

    feat: response_map filter for modifying filter chain responses
     * feat: source/extensions/filters/http/response_map: support disabling using per route config
    
       adopts the pattern set in source/extensions/filters/http/grpc_http1_reverse_bridge/*
    
     * feat: Add per-route response map config overrides
    
     * fix: response_map: major bugfix for a filter iteration with no request headers

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -37,6 +37,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/filters/common/rbac:88.1"
 "source/extensions/filters/http/cache:93.4"
 "source/extensions/filters/http/cache/simple_http_cache:96.0"
+"source/extensions/filters/http/external_response:95.9"
 "source/extensions/filters/http/grpc_json_transcoder:94.7"
 "source/extensions/filters/http/ip_tagging:89.1"
 "source/extensions/filters/http/kill_request:95.3" # Death tests don't report LCOV
