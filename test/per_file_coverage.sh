bb1de981e334da88e1f17a22fc9d739b30b172ee
John Esmet <john.esmet@gmail.com>
2020-10-28 20:37:07 +0000

    response_map filter for modifying filter chain responses

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -39,6 +39,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/filters/http/cache:80.7"
 "source/extensions/filters/http/cache/simple_http_cache:84.5"
 "source/extensions/filters/http/dynamic_forward_proxy:94.9"
+"source/extensions/filters/http/external_response:95.9"
 "source/extensions/filters/http/ip_tagging:91.2"
 "source/extensions/filters/http/grpc_json_transcoder:93.3"
 "source/extensions/filters/http/oauth2:96.5"
