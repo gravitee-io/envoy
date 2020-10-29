073098dba628d16937b5665474820ace8382ff98
John Esmet <john.esmet@gmail.com>
2020-10-29 18:51:09 +0000

    Add per-route response map config overrides

--- source/common/response_map/response_map.h
+++ source/common/response_map/response_map.h
@@ -44,11 +44,12 @@ public:
    */
   static ResponseMapPtr
   create(const envoy::extensions::filters::http::response_map::v3::ResponseMap& config,
-         Server::Configuration::FactoryContext& context);
+         Server::Configuration::CommonFactoryContext& context,
+         ProtobufMessage::ValidationVisitor& validationVisitor);
 
   /**
    * Create a default ResponseMap object with empty config.
-   * It is used at places without Server::Configuration::FactoryContext.
+   * It is used at places without Server::Configuration::CommonFactoryContext.
    */
   static ResponseMapPtr createDefault();
 };
