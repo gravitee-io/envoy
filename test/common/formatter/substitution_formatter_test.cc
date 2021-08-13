8393d6209fddc4ebb7f8032f932376509d1a7f23
Luke Shumaker <lukeshu@datawire.io>
2021-08-13 04:13:56 -0600

    nit: substitution_formatter_test.cc: fix typo: formart -> format

--- test/common/formatter/substitution_formatter_test.cc
+++ test/common/formatter/substitution_formatter_test.cc
@@ -1617,23 +1617,23 @@ TEST(SubstitutionFormatterTest, DownstreamPeerCertVStartFormatter) {
   // No downstreamSslConnection
   {
     EXPECT_CALL(stream_info, downstreamSslConnection()).WillRepeatedly(Return(nullptr));
-    DownstreamPeerCertVStartFormatter cert_start_formart("DOWNSTREAM_PEER_CERT_V_START(%Y/%m/%d)");
-    EXPECT_EQ(absl::nullopt, cert_start_formart.format(request_headers, response_headers,
-                                                       response_trailers, stream_info, body));
-    EXPECT_THAT(cert_start_formart.formatValue(request_headers, response_headers, response_trailers,
-                                               stream_info, body),
+    DownstreamPeerCertVStartFormatter cert_start_format("DOWNSTREAM_PEER_CERT_V_START(%Y/%m/%d)");
+    EXPECT_EQ(absl::nullopt, cert_start_format.format(request_headers, response_headers,
+                                                      response_trailers, stream_info, body));
+    EXPECT_THAT(cert_start_format.formatValue(request_headers, response_headers, response_trailers,
+                                              stream_info, body),
                 ProtoEq(ValueUtil::nullValue()));
   }
   // No validFromPeerCertificate
   {
-    DownstreamPeerCertVStartFormatter cert_start_formart("DOWNSTREAM_PEER_CERT_V_START(%Y/%m/%d)");
+    DownstreamPeerCertVStartFormatter cert_start_format("DOWNSTREAM_PEER_CERT_V_START(%Y/%m/%d)");
     auto connection_info = std::make_shared<Ssl::MockConnectionInfo>();
     EXPECT_CALL(*connection_info, validFromPeerCertificate()).WillRepeatedly(Return(absl::nullopt));
     EXPECT_CALL(stream_info, downstreamSslConnection()).WillRepeatedly(Return(connection_info));
-    EXPECT_EQ(absl::nullopt, cert_start_formart.format(request_headers, response_headers,
-                                                       response_trailers, stream_info, body));
-    EXPECT_THAT(cert_start_formart.formatValue(request_headers, response_headers, response_trailers,
-                                               stream_info, body),
+    EXPECT_EQ(absl::nullopt, cert_start_format.format(request_headers, response_headers,
+                                                      response_trailers, stream_info, body));
+    EXPECT_THAT(cert_start_format.formatValue(request_headers, response_headers, response_trailers,
+                                              stream_info, body),
                 ProtoEq(ValueUtil::nullValue()));
   }
   // Default format string
