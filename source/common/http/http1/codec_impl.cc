dcc3afef1680c1c4f932c73e0c9bca8b0b5db484
Aidan Hahn <aidanhahn@datawire.io>
2022-01-10 18:51:57 -0800

    feat(http1): adds Custom header rewrite rules
    By default, Envoy normalizes all HTTP headers
    when using http/1 so that they are lower cased. There are
    some configuration toggles so that it can leverage
    proper casing which is implied in RFC-7230. Although clients
    and servers should treat headers as case-insensitve some do not.
    
    This change provides a new field called "Custom" in the
    config.core.v3.Http1ProtocolOptions.HeaderKeyFormat which enables
    users to override case-insensitve headers so that they will match
    the configured casing.
    
    An example response header mapped from: x-my-header --> X-MY-Header
    
    This powers Emissary-ingress's `module.header_case_override` feature.
    
    No changes have been made since the original commits other than
    rewriting the commit message to provide historical context.
    
    Signed-off-by: Aidan Hahn <aidanhahn@datawire.io>
    Signed-off-by: Lance Austin <laustin@datawire.io>
    (cherry picked from commit 279ed8657de7f622ed6aa432d7c9e906e8f8c85c)
    (cherry picked from commit 793716d79e50db0c83993b4e41361613c750d976)

--- source/common/http/http1/codec_impl.cc
+++ source/common/http/http1/codec_impl.cc
@@ -71,8 +71,15 @@ const StringUtil::CaseUnorderedSet& caseUnorderdSetContainingUpgradeAndHttp2Sett
 }
 
 HeaderKeyFormatterConstPtr encodeOnlyFormatterFromSettings(const Http::Http1Settings& settings) {
-  if (settings.header_key_format_ == Http1Settings::HeaderKeyFormat::ProperCase) {
+  switch (settings.header_key_format_) {
+  case Http1Settings::HeaderKeyFormat::ProperCase:
     return std::make_unique<ProperCaseHeaderKeyFormatter>();
+
+  case Http1Settings::HeaderKeyFormat::Custom:
+    return std::make_unique<CustomHeaderKeyFormatter>(settings.header_key_format_rules_);
+
+  default:
+    break;
   }
 
   return nullptr;
